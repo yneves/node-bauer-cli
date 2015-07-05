2a7add46e962f6b889d80dcea04db1ccebdb679a
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-07-05 14:54:03 -0300

    init

---
+++ bauer-cli.js
@@ -0,0 +1,15 @@
+/*!
+**  bauer-cli -- Command-line interface for bauer-crawler.
+**  Copyright (c) 2015 Yuri Neves Silveira <http://yneves.com>
+**  Licensed under The MIT License <http://opensource.org/licenses/MIT>
+**  Distributed on <http://github.com/yneves/node-bauer-cli>
+*/
+// - -------------------------------------------------------------------- - //
+
+"use strict";
+
+var cli = require("./");
+
+cli.run();
+
+// - -------------------------------------------------------------------- - //
