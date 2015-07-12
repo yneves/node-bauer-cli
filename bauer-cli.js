c3fe32df8a00064aecbc134629450626e6b0a703
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-07-12 12:15:10 -0300

    make it simple

--- bauer-cli.js
+++ bauer-cli.js
@@ -1,3 +1,4 @@
+#! /usr/bin/env node
 /*!
 **  bauer-cli -- Command-line interface for bauer-crawler.
 **  Copyright (c) 2015 Yuri Neves Silveira <http://yneves.com>
@@ -8,8 +9,31 @@
 
 "use strict";
 
-var cli = require("./");
+var fs = require("fs");
+var path = require("path");
 
-cli.run();
+// - -------------------------------------------------------------------- - //
+
+var cwd = process.cwd();
+var arg = process.argv[2] || "./";
+var packageDir = path.resolve(cwd,arg);
+var packageFile = path.resolve(cwd,arg,"package.json");
+var crawlerDir = path.resolve(packageDir,"node_modules/bauer-crawler");
+
+fs.exists(packageFile,function(packageFileExists) {
+  if (packageFileExists) {
+    fs.exists(crawlerDir,function(crawlerDirExists) {
+      if (crawlerDirExists) {
+        var Crawler = require(crawlerDir);
+        var crawler = new Crawler(packageFile);
+        crawler.start(packageDir);
+      } else {
+        throw new Error("bauer-crawler not found");
+      }
+    });
+  } else {
+    throw new Error("package.json not found");
+  }
+});
 
 // - -------------------------------------------------------------------- - //
