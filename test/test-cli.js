0f63e3b6a066a17b59ff0d173f7572db3cda0408
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-09-20 14:58:26 -0300

    load from script

--- test/test-cli.js
+++ test/test-cli.js
@@ -12,10 +12,9 @@ describe("cli",function() {
   it("constructor",function() {
     assert.strictEqual(typeof cli.add,"function");
     assert.strictEqual(typeof cli.remove,"function");
-    assert.strictEqual(typeof cli.new,"function");
+    assert.strictEqual(typeof cli.init,"function");
     assert.strictEqual(typeof cli.run,"function");
   });
-  
   
 });
 
